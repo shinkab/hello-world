d4ab08ec591c682a2f1e15040cb5cdeb773e5c3f
ShinKab.Kim <ShinKab.Kim@doftech.co.kr>
2021-01-25 10:13:10 +0900

    Add some files

---
+++ hello.py
@@ -0,0 +1,24 @@
+print("Hello world")
+
+x = 0
+if x > 0:
+    a = 1
+    b = 2
+    c = a + b
+else:
+    a = -1
+    b = -2
+    c = a - b
+
+print (c)
+
+import math
+
+n = math.sqrt(9.0)
+
+print (n)
+
+# 코멘트1
+print(1)
+
+print(2) # 코멘트2
